ac75b868b02d699343e8ef3439401d3dc1c2358c
user <user@users-MacBook-Pro.local>
2018-09-16 12:05:38 +0600

    hw2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "App.css";
+
 
 class App extends Component {
   render() {
-    return (
+    return ( 
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
